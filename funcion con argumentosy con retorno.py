1ef7ede02cd2b0a37102d0c670d324aa4650e0b8
shinneiperlasilva <perl.silva@duocuc.cl>
2024-06-17 10:47:49 -0400

    Add files via upload

---
+++ funcion con argumentosy con retorno.py
@@ -0,0 +1,21 @@
+import os 
+os.system("cls")
+def sumrnuemro():
+    a=22
+    b=12
+    c=100
+    return(a+b+c)
+    def sumarNumeros1():
+        while(True):
+            try:
+                a=int(input("Ingrse un nuemro+"))
+                break
+            except ValueError:
+                print("error; debe ser un numero entero")
+            return(a+b)
+    #principal
+    os.system ("cls")
+    a=sumarNumeros()
+    print("La suma es:" , a)
+    cc=sumarNumeros1
+    print("La suma es la segunda funcion es:,cc")
